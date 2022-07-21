e72bc7a0c3fc406dd85d48b5f5c957a2dfa3b5e1
kimgyeongseo <gyeongseo.kim@avikus.ai>
2022-07-21 09:24:55 +0900

    [SPC-715] feat: task-row 컴포넌트 수정 및 task-list 작성

---
+++ frontend/src/todo/components/task-row/task-row.test.tsx
@@ -0,0 +1,62 @@
+import { render, screen } from "@testing-library/react"
+import userEvent from "@testing-library/user-event"
+
+import { TaskProps } from "../task-row/task-row"
+import Todo from "./task-row"
+
+describe("<Todo />", () => {
+  const changeTest = jest.fn()
+  const deleteTest = jest.fn()
+  const sample: TaskProps = {
+    id: "1",
+    title: "task 1",
+    isChecked: false,
+  }
+
+  it("renders task", () => {
+    render(<Todo task={sample} handleTaskComplete={changeTest} handleTaskDelete={deleteTest} />)
+    const taskCheckbox = screen.getByRole("checkbox")
+    const title = screen.getByLabelText(sample.title).id
+    const deleteButton = screen.getByRole("button")
+
+    expect(taskCheckbox).toBeInTheDocument()
+    expect(title).toEqual("task 1")
+    expect(deleteButton).toBeInTheDocument()
+  })
+
+  it("calls handleTaskComplete when checkbox clicked", () => {
+    render(<Todo task={sample} handleTaskComplete={changeTest} handleTaskDelete={deleteTest} />)
+
+    const taskCheckbox = screen.getByRole("checkbox")
+    userEvent.click(taskCheckbox)
+
+    expect(changeTest).toBeCalledTimes(1)
+  })
+
+  it("completes task when true value comes", () => {
+    sample.isChecked = true
+
+    render(<Todo task={sample} handleTaskComplete={changeTest} handleTaskDelete={deleteTest} />)
+    const taskCheckbox = screen.getByRole("checkbox") as HTMLInputElement
+
+    expect(taskCheckbox.checked).toEqual(true)
+  })
+
+  it("is not complete task when false value comes", () => {
+    sample.isChecked = false
+
+    render(<Todo task={sample} handleTaskComplete={changeTest} handleTaskDelete={deleteTest} />)
+    const taskCheckbox = screen.getByRole("checkbox") as HTMLInputElement
+
+    expect(taskCheckbox.checked).toEqual(false)
+  })
+
+  it("calls handleTaskDelete when button clicked", () => {
+    render(<Todo task={sample} handleTaskComplete={changeTest} handleTaskDelete={deleteTest} />)
+
+    const deleteButton = screen.getByRole("button")
+    userEvent.click(deleteButton)
+
+    expect(deleteTest).toBeCalledWith(sample.id)
+  })
+})
---
+++ frontend/src/todo/components/task-row/task-row.tsx
@@ -0,0 +1,32 @@
+import React, { useState } from "react"
+
+export interface TaskProps {
+  id: string
+  title: string
+  isChecked: boolean
+}
+
+export interface TaskItemProps {
+  task: TaskProps
+  handleTaskComplete: (event: React.ChangeEvent<HTMLInputElement>, id: string) => void
+  handleTaskDelete: (id: string) => void
+}
+
+const Todo = (props: TaskItemProps) => {
+  return (
+    <div>
+      <input
+        type="checkbox"
+        id={props.task.title}
+        checked={props.task.isChecked}
+        onChange={(event: React.ChangeEvent<HTMLInputElement>) => props.handleTaskComplete(event, props.task.id)}
+      />
+      <label htmlFor={props.task.title}>
+        <span style={{ textDecoration: props.task.isChecked ? "line-through" : "none" }}>{props.task.title}</span>
+      </label>
+      <button onClick={() => props.handleTaskDelete(props.task.id)}>X</button>
+    </div>
+  )
+}
+
+export default Todo
